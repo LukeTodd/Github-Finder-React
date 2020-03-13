27f80571e9b32c1191bb886140767bb15f6c3214
UrbanLink 5 <urbanlink5@UrbanLinks-iMac.local>
2020-03-13 12:14:14 -0600

    Learn props and proptypes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React, { Fragment, Component } from "react";
 import "./App.css";
+import "./components/layout/Navbar";
+import Navbar from "./components/layout/Navbar";
 
 class App extends Component {
-  foo = () => "Bars";
-
   render() {
-    let name = "John Doe";
-    let loading = false;
-    let showName = true;
-
-    // if (loading) {
-    //   return <h4>Loading...</h4>;
-    // }
-
-    let foo = () => "Bar";
-
     return (
       <div className='App'>
-        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
+        <Navbar />
       </div>
     );
   }
