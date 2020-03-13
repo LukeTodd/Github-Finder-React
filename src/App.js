e8781882ee9b1c6694a5ee9209df2ed37596b83c
UrbanLink 5 <urbanlink5@UrbanLinks-iMac.local>
2020-03-13 11:29:52 -0600

    Learning Expression and Conditionals

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React, { Component } from "react";
+import React, { Fragment, Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  foo = () => "Bars";
+
   render() {
+    let name = "John Doe";
+    let loading = false;
+    let showName = true;
+
+    // if (loading) {
+    //   return <h4>Loading...</h4>;
+    // }
+
+    let foo = () => "Bar";
+
     return (
       <div className='App'>
-        <h1>Hello From React</h1>
+        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
       </div>
     );
   }
