e6eaef90cafe2d7d8b2c39b60d209f87fa67309a
Luke Todd <luke.todd22@yahoo.com>
2020-03-14 22:02:23 -0600

    Spinner & Global Variables

--- src/App.js
+++ src/App.js
@@ -13,7 +13,9 @@ class App extends Component {
   async componentDidMount() {
     this.setState({ loading: true });
 
-    let res = await axios.get('https://api.github.com/users');
+    let res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
 
     this.setState({ users: res.data, loading: false });
   }
@@ -23,7 +25,7 @@ class App extends Component {
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Users loading={this.state.loading} users={this.state.users}/>
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
